5adc513e68f339ec1970769d1cf97295409d5143
TailsHu3Hu3 <tailshu3hu3@gmail.com>
2024-12-06 14:27:25 -0300

    Adição do novo JS + Segunda section finalizada (CSS/HTML)

---
+++ js/shareScript.js
@@ -0,0 +1,60 @@
+const productsGrid = document.querySelector('.productsBoxesWrapper')
+const homeButton = document.querySelector('#redirectHome')
+// Copiado do script.js 
+let infoProducts = ''
+let nextPageAPI = 'https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1'
+let firstPageAPI = ''
+
+function transformJson(dataString) {
+    return dataString.json()
+}
+
+function arrangingValues(dataJson) {
+    firstPageAPI = dataJson
+    infoProducts = firstPageAPI.products
+    nextPageAPI = 'https://' + firstPageAPI.nextPage
+    createValues()
+}
+
+function fetchError() {
+    console.log("Something went wrong.")
+}
+
+function handlingAPI() {
+    firstPageAPI = fetch(nextPageAPI)
+            .then(transformJson)
+            .then(arrangingValues)
+            .catch(fetchError)
+}
+
+function createValues() {
+    for (let conectorProducts of infoProducts) {
+        productsGrid.innerHTML += `
+        <article>
+            <figure>
+                <div class="imageWrapper">
+                    <img src="${conectorProducts.image}" alt="">
+                </div>
+                <figcaption>
+                    <span>${conectorProducts.name}</span>
+                </figcaption>
+            </figure>
+            <p class="textBoxProducts">
+                ${conectorProducts.description}
+            </p>
+    
+            <div class="pricesBoxProducts">
+                <span class="mediaNameBoxProducts">${conectorProducts.name}</span>
+                <span class="priceOrigin">De: R$${conectorProducts.oldPrice}</span>
+                <span class="presentPrice">Por: R$${conectorProducts.price}</span>
+                <span class="portionPrice">ou ${conectorProducts.installments.count}x de R$${conectorProducts.installments.value}</span>
+                <button type="button" class="mediaProductsButtonBuy">Comprar</button>
+            </div>
+                    
+            <button type="button" class="productsButtonBuy">Comprar</button>
+        </article>
+        `
+    }
+}
+
+handlingAPI()
